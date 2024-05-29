fff5aae3d183866405fef537f9b7283c240fca14
arijzhou <ariszh6@gmail.com>
2024-05-29 01:13:46 -0700

    Stored stats of models that complete the level; created while and if loops that identify the best 2 models that most effectively complete, or survive, the level

--- Model.py
+++ Model.py
@@ -27,10 +27,46 @@ def make_model(initializer):
 
 initializer = tf.keras.initializers.HeNormal(seed = 1)
 fitness = []
+best_models = []
 
 for i in range(10):
         model = make_model(initializer)
         print(model.predict(np.array([[1,1,1,1,1,1,1]])))
         fitness.append(Game_model_version.Game(window,model))
+       
+        if fitness[i][0] == True:
+            best_models.append(fitness[i]) 
 
 print(fitness)
+print("")  
+
+while len(best_models) > 2: # if more than 2 models find a solution, find the best 2
+    max_val, ind = -1,0     
+        
+    for i in range(len(best_models)):
+        if best_models[i][2] > max_val:
+            max_val, ind = best_models[i][2], i
+    
+    del best_models[ind]
+    
+
+if len(best_models) < 2:  # other cases: 0 models solve, 1 model solves (find two models where kirby survives longest)
+    if len(best_models) == 0:
+        max_val, ind = -1,0
+        
+        for i in len(fitness):
+            if fitness[i][2] > max_val:
+                max_val, ind = fitness[i][2], i
+                
+        best_models.append(fitness[i])
+        del fitness[i]
+        
+    max_val, ind = -1,0
+        
+    for i in len(fitness):
+        if fitness[i][2] > max_val:
+             max_val, ind = fitness[i][2], i
+                
+    best_models.append(fitness[i])
+        
+print(best_models) 
